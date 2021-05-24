9ee31ef762e3d775a3efbef4b783cf5ccae675f6
Albert Suntic <albert@developer.its.me>
2021-05-24 10:26:13 +0200

    Split Fiscal class and service tests

---
+++ tests/FiscalServiceTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests;
+
+use Carbon\Carbon;
+use DeveloperItsMe\FiscalService\Models\CashDeposit;
+use PHPUnit\Framework\TestCase;
+
+class FiscalServiceTest extends TestCase
+{
+    use HasTestData;
+
+    /** @skip */
+    public function it_can_send_tcr_request()
+    {
+        $response = $this->fiscal()
+            ->request($this->getRegisterTCRRequest())
+            ->send();
+
+        $this->assertTrue($response->valid(), $response->error());
+    }
+
+    /** @skip */
+    public function it_can_send_initial_cash_deposit_request()
+    {
+        $date = Carbon::now('Europe/Podgorica')->format('Y-m-d');
+
+        if ($date == @file_get_contents('./tests/it_can_send_initial_cash_deposit_request.skip')) {
+            $this->markTestSkipped('Already sent initial cash deposit request');
+        }
+
+        $responseInitial = $this->fiscal()
+            ->request($this->getRegisterCashDepositRequest())
+            ->send();
+
+        file_put_contents('./tests/it_can_send_initial_cash_deposit_request.skip', $date);
+
+        $this->assertTrue($responseInitial->valid(), $responseInitial->error());
+    }
+
+    /** @skip */
+    public function it_can_send_withdraw_cash_deposit_request()
+    {
+        $responseWithdraw = $this->fiscal()
+            ->request($this->getRegisterCashDepositRequest(CashDeposit::OPERATION_WITHDRAW))
+            ->send();
+
+        $this->assertTrue($responseWithdraw->valid(), $responseWithdraw->error());
+    }
+
+    /** @skip */
+    public function it_can_send_invoice_request()
+    {
+        $response = $this->fiscal()
+            ->request($this->getRegisterInvoiceRequest())
+            ->send();
+
+        $this->assertTrue($response->valid(), $response->error());
+    }
+}
--- tests/FiscalTest.php
+++ tests/FiscalTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests;
 
-use Carbon\Carbon;
-use DeveloperItsMe\FiscalService\Models\CashDeposit;
 use DeveloperItsMe\FiscalService\Models\Invoice;
 use PHPUnit\Framework\TestCase;
 
@@ -40,53 +38,5 @@ class FiscalTest extends TestCase
 
         $this->fiscal(null, false);
         $this->assertEquals($this->qrProductionUrl, Invoice::$qrBaseUrl);
-    }
-
-    /** @skip */
-    public function it_can_send_tcr_request()
-    {
-        $response = $this->fiscal()
-            ->request($this->getRegisterTCRRequest())
-            ->send();
-
-        $this->assertTrue($response->valid(), $response->error());
-    }
-
-    /** @skip */
-    public function it_can_send_initial_cash_deposit_request()
-    {
-        $date = Carbon::now('Europe/Podgorica')->format('Y-m-d');
-
-        if ($date == @file_get_contents('./tests/it_can_send_initial_cash_deposit_request.skip')) {
-            $this->markTestSkipped('Already sent initial cash deposit request');
-        }
-
-        $responseInitial = $this->fiscal()
-            ->request($this->getRegisterCashDepositRequest())
-            ->send();
-
-        file_put_contents('./tests/it_can_send_initial_cash_deposit_request.skip', $date);
-
-        $this->assertTrue($responseInitial->valid(), $responseInitial->error());
-    }
-
-    /** @skip */
-    public function it_can_send_withdraw_cash_deposit_request()
-    {
-        $responseWithdraw = $this->fiscal()
-            ->request($this->getRegisterCashDepositRequest(CashDeposit::OPERATION_WITHDRAW))
-            ->send();
-
-        $this->assertTrue($responseWithdraw->valid(), $responseWithdraw->error());
-    }
-
-    /** @skip */
-    public function it_can_send_invoice_request()
-    {
-        $response = $this->fiscal()
-            ->request($this->getRegisterInvoiceRequest())
-            ->send();
-
-        $this->assertTrue($response->valid(), $response->error());
     }
 }
