76b4d56576c191e0926c1686352e46161ef0eeb7
Albert Suntic <albert@developer.its.me>
2021-05-24 14:57:01 +0200

    Fix cert test

--- tests/CertificateTest.php
+++ tests/CertificateTest.php
@@ -7,10 +7,12 @@ use PHPUnit\Framework\TestCase;
 
 class CertificateTest extends TestCase
 {
+    use HasTestData;
+
     /** @test */
     public function it_reads_certificate()
     {
-        $cert = new Certificate('./CoreitPotpisSoft.pfx', '123456');
+        $cert = new Certificate($this->certPath, $this->certPassphrase);
 
         $this->assertNotFalse($cert->getPrivateKey());
         $this->assertNotFalse($cert->getPublicData());
